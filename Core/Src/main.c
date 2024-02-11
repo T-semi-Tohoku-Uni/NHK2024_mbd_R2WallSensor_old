cefc2077c317bc9bc35c408b304f7dac765ee972
TsemiYuta <43599353+yuppy1005@users.noreply.github.com>
2024-02-11 17:12:22 +0900

    Revert "基幹CAN開通！！！"
    This reverts commit 11665475b8af5da73d18375799b617cea4f3c146.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -65,7 +65,6 @@ static void MX_TIM17_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 void  HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 	if(htim == &htim17){
 		uint8_t TxData = 0;
@@ -134,11 +133,10 @@ int main(void)
   MX_FDCAN1_Init();
   MX_TIM17_Init();
   /* USER CODE BEGIN 2 */
-  HAL_FDCAN_Start(&hfdcan1);
   HAL_TIM_Base_Start_IT(&htim17);
   printf("Initialized\r\n");
 
-
+  HAL_FDCAN_Start(&hfdcan1);
 
   /* USER CODE END 2 */
 
